6cfd735d17f6b9cfb602c8e5f9ff419862102e8c
dudu.clx <dudu.clx@hotmail.fr>
2016-12-04 23:22:03 +0100

    Create install_xscreensaver.sh

---
+++ install_xscreensaver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#get xcreensaver
+sudo apt-get install xscreensaver
+
+#get glslideshow
+sudo apt-get install xscreensaver-gl-extra xscreensaver-data-extra
+
+#you need to go preferences and then screensaver.
+#If you want to only run a slideshow, then select GLSlideshow in "Only One Screen Saver" mode.
