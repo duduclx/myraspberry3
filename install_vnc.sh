ac41ae7d0c06ed5bd4eda6eed96f83665ca62f36
dudu.clx <dudu.clx@hotmail.fr>
2016-12-05 00:31:08 +0100

    Update install_vnc.sh

--- install_vnc.sh
+++ install_vnc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#via web and java applet
-#go to ip:5800
-
 #install tightvncserver
 sudo apt-get install tightvncserver
 
@@ -14,18 +11,3 @@ sudo update-rc.d vncserver enable
 
 #run vnc
 sudo service vncserver start
-
-#### other method, not tested
-#enable service
-#sudo update-rc.d vncserver-x11-serviced defaults
-#sudo update-rc.d vncserver-x11-serviced enable
-
-#install
-#sudo apt-get install x11vnc
-
-#get ip
-#hostname -I >> ip.txt
-#IP="$( cat ip.txt)"
-
-#make it listen forever
-#su - pi -c '/usr/bin/x11vnc -display :0 -noxdamage -shared -usepw -listen -forever $IP' 
